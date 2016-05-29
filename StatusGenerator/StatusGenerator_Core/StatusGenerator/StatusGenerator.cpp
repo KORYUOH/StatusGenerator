a649d52573232910964a34422acdea120a4f5503
KORYUOH <coyamasss@yahoo.co.jp>
2016-05-29 14:33:06 +0900

    Add Core program

---
+++ StatusGenerator/StatusGenerator_Core/StatusGenerator/StatusGenerator.cpp
@@ -0,0 +1,72 @@
+﻿#include "StatusGenerator.h"
+#include	<random>
+using namespace std;
+
+//================================================================================
+// コンストラクタ
+IStatusGenerator::IStatusGenerator()
+{
+}
+
+
+//================================================================================
+// 生成シードの生成
+void IStatusGenerator::SetSeed( const std::string& seed )
+{
+
+	if( HasEngine() )
+	{
+		FreeEngine();
+	}
+	
+}
+
+//================================================================================
+// 値取得
+int IStatusGenerator::Roll( const struct GenerateLimit& limit )
+{
+	if( !HasEngine() )
+	{
+		RandomInit();
+	}
+	std::uniform_int_distribution<int> mDistribution(limit.MinLimit , limit.MaxLimit);
+
+	return mDistribution( *mEngine );
+}
+
+//================================================================================
+// エンジンを持っているか
+bool IStatusGenerator::HasEngine()const
+{
+	return nullptr != mEngine;
+}
+
+//================================================================================
+// エンジンをランダム初期化
+void IStatusGenerator::RandomInit()
+{
+	if( HasEngine() )
+	{
+		FreeEngine();
+	}
+
+	mEngine = new mt19937( std::random_device() );
+
+}
+
+//================================================================================
+// エンジンを開放
+void IStatusGenerator::FreeEngine()
+{
+	mEngine = nullptr;
+}
+
+
+
+
+
+
+
+
+
+
---
+++ StatusGenerator/StatusGenerator_Core/StatusGenerator/StatusGenerator.h
@@ -0,0 +1,76 @@
+﻿/**============================================================
+ * @file	StatusGenerator.h
+ * @brief	ステータス自動生成
+ * @author	KORYUOH
+ * @version	0.01
+============================================================*/
+#ifndef	_STATUS_GENERATOR_H_
+#define	_STATUS_GENERATOR_H_
+#pragma once
+
+#include	<string>
+#include	<random>
+
+struct GenerateLimit
+{
+	int MinLimit;
+	int MaxLimit;
+};
+
+class IStatusGenerator
+{
+	public:
+		/**
+		 * @brief	コンストラクタ
+		 */
+		IStatusGenerator();
+		
+		/**
+		 * @brief	デストラクタ
+		 */
+		virtual ~IStatusGenerator(){};
+
+		/**
+		 * @brief	生成
+		 */
+		virtual void Generate() = 0;
+
+		/**
+		 * @brief	生成シードの設定
+		 * @param[in]	seed : データシード文字列
+		 */
+		void SetSeed( const std::string& seed );
+		
+	protected:
+
+		/**
+		 * @brief	値取得
+		 * @param[in]	limit : 取得限界
+		 */
+		int Roll( const struct GenerateLimit& limit );
+
+		/**
+		 * @brief	エンジンを持っているか？
+		 * @return 持っていればtrue
+		 */
+		bool HasEngine()const;
+
+		/**
+		 * @brief	エンジンランダム初期化
+		 */
+		void RandomInit();
+
+		/**
+		 * @brief	エンジンを開放
+		 */
+		void FreeEngine();
+
+	private:
+	
+		///エンジン
+		std::mt19937* mEngine;
+};
+
+
+
+#endif
