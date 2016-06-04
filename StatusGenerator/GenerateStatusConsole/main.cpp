c74f6e1f0d5c124d884b3300b0429825263430bc
Coyama <koyamay@marv.jp>
2016-06-04 18:57:12 +0900

    test code

--- StatusGenerator/GenerateStatusConsole/main.cpp
+++ StatusGenerator/GenerateStatusConsole/main.cpp
@@ -1,4 +1,5 @@
 ﻿#include	<iostream>
+#include	<fstream>
 #include	"const.h"
 #include	"../StatusGenerator/StatusGenerator.h"
 using namespace std;
@@ -45,5 +46,84 @@ int main(int argc, char* argv[])
 	cout << "HP : " <<	p.hp	<< endl;
 	cout << "ATK : " <<	p.atk	<< endl;
 	cout << "DEF : " <<	p.def	<< endl;
+
+	int size = sizeof( p ) / sizeof( char );
+	cout << "size : " << size << endl;
+	cout << " S Addr : 0x" << &p << endl;
+	cout << " E Addr : 0x" << &p+sizeof(p) << endl;
+	ofstream ofs;
+	ofstream ofs2;
+	ofs.open("data.dat" , ios::out | ios::binary );
+	ofs2.open("data2.dat" , ios::out | ios::binary );
+	for( int i = 0 ; i  < size ; ++i )
+	{
+		cout << (char)(&p+sizeof(char)*i) << endl;
+		//ofs << (char)(&p+sizeof(char)*i);
+		//cout << (char)(&p+sizeof(char)*i) << endl;
+	}
+	ofs.write( (char*) &p , sizeof(p) );
+	ofs.close();
+
+	{
+		Params x = p;
+		char* ptr = (char*)&x;
+		for( int i = 0 ; i < size ; ++i )
+		{
+			*ptr ^= 'a';
+			++ptr;
+		}
+		ofs2.write( (char*) &x, sizeof( x ) );
+		ofs2.close();
+	}
+
+	Params t = {0,0,0};
+	ifstream ifs;
+	ifs.open("data.dat" , ios::in|ios::binary);
+	if( !ifs.fail() )
+	{
+		cout << "T" << endl;
+		cout << "HP : " <<	t.hp	<< endl;
+		cout << "ATK : " <<	t.atk	<< endl;
+		cout << "DEF : " <<	t.def	<< endl;
+		int siz = sizeof( t ) / sizeof( char );
+		//for( int i = 0; i < siz ; ++i )
+		//{
+			ifs.read( (char*) &t , sizeof(t) );
+		//}
+		cout << "T============" << endl;
+		cout << "HP : " <<	t.hp	<< endl;
+		cout << "ATK : " <<	t.atk	<< endl;
+		cout << "DEF : " <<	t.def	<< endl;
+
+	}
+	ifs.close();
+	
+	Params r = {0,0,0};
+	ifstream ifs2;
+	ifs2.open("data2.dat" , ios::in|ios::binary);
+	if( !ifs2.fail() )
+	{
+		cout << "R" << endl;
+		cout << "HP : " <<	r.hp	<< endl;
+		cout << "ATK : " <<	r.atk	<< endl;
+		cout << "DEF : " <<	r.def	<< endl;
+		int siz = sizeof( r ) / sizeof( char );
+			ifs2.read( (char*) &r , sizeof(r) );
+			char* u = (char*)&r;
+		for( int i = 0; i < siz ; ++i )
+		{
+			*u ^= 'a';
+			++u;
+		}
+		cout << "R============" << endl;
+		cout << "HP : " <<	r.hp	<< endl;
+		cout << "ATK : " <<	r.atk	<< endl;
+		cout << "DEF : " <<	r.def	<< endl;
+
+	}
+	ifs2.close();
+
+
+
 	return 0;
 }
