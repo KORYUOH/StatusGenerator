a2bc5c8b19178996818200b9f3216009283b0122
Coyama <koyamay@marv.jp>
2016-06-10 16:27:44 +0900

    AddGUI

---
+++ StatusGenerator/GenerateStatusGUI/main.cpp
@@ -0,0 +1,12 @@
+﻿#include <windows.h>
+#include <stdlib.h>
+#include <string.h>
+#include <tchar.h>
+
+//LRESULT CALLBACK WndProc( HWND hwnd , UINT msg , WPARAM wp , LPARAM lp );
+
+int WINAPI WinMain( HINSTANCE hInstance , HINSTANCE hPrevInstance , LPSTR lpCmdLine , int nCmdShow )
+{
+
+	return 0;
+}
